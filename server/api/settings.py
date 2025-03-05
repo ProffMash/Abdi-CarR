275a2d53a8c2382cc24a9b097b411d663d777291
david <kdavidmacharia8485@gmail.com>
2025-03-05 13:30:27 +0300

    render api

--- server/api/settings.py
+++ server/api/settings.py
@@ -25,12 +25,12 @@ SECRET_KEY = "django-insecure-+3b8x#%x!9dkmmr4^@6n@+im1bs=gu-ks*sy6#^87o$sx5-war
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['harars.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['abdis.onrender.com', '127.0.0.1', 'localhost']
 
 CORS_ALLOWED_ORIGINS = [
    'http://localhost:5173',
-   'https://hararhospital.vercel.app',
-   'https://harars.onrender.com',
+   'https://carrental-ashen.vercel.app',
+   'https://abdis.onrender.com',
 ]
 
 CORS_ALLOW_HEADERS = [
